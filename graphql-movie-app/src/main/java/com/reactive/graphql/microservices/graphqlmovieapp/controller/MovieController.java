caeec15b02f1bce831d81cd3a1c78cb9848fde8c
Sashank <sashanka.samantray@rabobank.nl>
2024-07-26 16:10:26 +0200

    query mapping completed

--- graphql-movie-app/src/main/java/com/reactive/graphql/microservices/graphqlmovieapp/controller/MovieController.java
+++ graphql-movie-app/src/main/java/com/reactive/graphql/microservices/graphqlmovieapp/controller/MovieController.java
@@ -2,8 +2,18 @@ package com.reactive.graphql.microservices.graphqlmovieapp.controller;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 import com.reactive.graphql.microservices.graphqlmovieapp.client.MovieRestClient;
+import com.reactive.graphql.microservices.graphqlmovieapp.model.Customer;
+import com.reactive.graphql.microservices.graphqlmovieapp.model.Genre;
+import com.reactive.graphql.microservices.graphqlmovieapp.model.Movie;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,6 +21,24 @@ public class MovieController {
 
     private final MovieRestClient movieRestClient;
 
+    @SchemaMapping(typeName = "UserProfile")
+    public Flux<Movie> watchList(Customer customer) {
+        return movieRestClient.moviesByIds(customer.getWatchList());
+    }
 
+    @SchemaMapping(typeName = "UserProfile")
+    public Flux<Movie> recommended(Customer customer) {
+        return movieRestClient.moviesByGenre(customer.getFavoriteGenre());
+    }
 
+    @QueryMapping
+    public Mono<Movie> movieDetails(@Argument(name = "id") Integer movieId) {
+        return movieRestClient.moviesByIds(List.of(movieId))
+                .next();
+    }
+
+    @QueryMapping
+    public Flux<Movie> moviesByGenre(@Argument(name = "genre") Genre favoutiteGenre) {
+        return movieRestClient.moviesByGenre(favoutiteGenre);
+    }
 }
--- graphql-movie-app/src/main/java/com/reactive/graphql/microservices/graphqlmovieapp/controller/ReviewController.java
+++ graphql-movie-app/src/main/java/com/reactive/graphql/microservices/graphqlmovieapp/controller/ReviewController.java
@@ -3,11 +3,20 @@ package com.reactive.graphql.microservices.graphqlmovieapp.controller;
 import lombok.RequiredArgsConstructor;
 
 import com.reactive.graphql.microservices.graphqlmovieapp.client.ReviewRestClient;
+import com.reactive.graphql.microservices.graphqlmovieapp.model.Movie;
+import com.reactive.graphql.microservices.graphqlmovieapp.model.Review;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 
 @Controller
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewRestClient reviewRestClient;
+
+    @SchemaMapping(typeName = "MovieDetails")
+    public Flux<Review> reviews(Movie movie) {
+        return this.reviewRestClient.movieReviews(movie.getId());
+    }
 }
